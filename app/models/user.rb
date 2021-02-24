903a63003496cc6bd44aeab7d6f1ede9b1a01ff1
Alexis Varela <alexisbec13@hotmail.com>
2021-02-24 13:50:14 -0600

    fixed linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: { case_sensitive: true }, length: { maximum: 50 }
-  EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: EMAIL_FORMAT }
 
   has_many :posts
